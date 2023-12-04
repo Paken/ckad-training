8c4a0bad778980b742e17b2f0c12be1dc801f5d1
Paken <fjbp1@hotmail.com>
2023-12-04 16:02:24 +0100

    fixed check script

--- 02.- ReplicaSets/check-rs.sh
+++ 02.- ReplicaSets/check-rs.sh
@@ -4,11 +4,7 @@ desired=$(kubectl get rs | grep rs01 | awk '{print $2}')
 current=$(kubectl get rs | grep rs01 | awk '{print $3}')
 ready=$(kubectl get rs | grep rs01 | awk '{print $4}')
 
-echo $desired
-echo $current
-echo $ready
-
-if [ $desired == $current ] && [ $desired == $ready ] && [ $ready != 0 ];
+if [ $desired == $current ] && [ $desired == $ready ] && [ $ready <> 0 ];
 then
     echo "The Replica Set is running correctly"
 else
