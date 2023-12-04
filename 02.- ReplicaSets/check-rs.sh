80db8468e3971008672ca23aebeeb1d030ab3a1a
Paco <pakken@Pacos-MacBook-Pro.local>
2023-12-04 12:21:40 +0100

    Added replica set exercise

---
+++ 02.- ReplicaSets/check-rs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+desired=$(kubectl get rs | grep rs01 | awk '{print $2}')
+current=$(kubectl get rs | grep rs01 | awk '{print $3}')
+ready=$(kubectl get rs | grep rs01 | awk '{print $4}')
+
+echo $desired
+echo $current
+echo $ready
+
+if [ $desired == $current ] && [ $desired == $ready ] && [ $ready != 0 ];
+then
+    echo "The Replica Set is running correctly"
+else
+    echo "The Replica Set is NOT running correctly"
+fi
+
